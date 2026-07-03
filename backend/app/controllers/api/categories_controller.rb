b07568a35cc8ef82ba6a318355e22376a5bf21c2
darrylemiles <dmilesbacay@gmail.com>
2026-07-04 02:02:38 +0800

    feat(category): implement category management API
    Refs to FEATURE-001

--- backend/app/controllers/api/categories_controller.rb
+++ backend/app/controllers/api/categories_controller.rb
@@ -3,4 +3,30 @@ class Api::CategoriesController < ApplicationController
     categories = Category.order(:name)
     render json: categories
   end
+
+  def create
+    category = Category.new(category_params)
+
+    if category.save
+      render json: category, status: :created
+    else
+      render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+
+    if category.destroy
+      head :no_content
+    else
+      render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
